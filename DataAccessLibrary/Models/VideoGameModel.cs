84c46c72265c8dfa9f184fc4dbe94af87334b31d
adamcox37 <adam_mcox@yahoo.com>
2021-01-01 20:45:15 -0500

    Starting CRUD

--- DataAccessLibrary/Models/VideoGameModel.cs
+++ DataAccessLibrary/Models/VideoGameModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DataAccessLibrary.Models
@@ -7,8 +8,13 @@ namespace DataAccessLibrary.Models
     public class VideoGameModel
     {
         public int Id { get; set; }
+
+        [Display(Name = "Game Title")]
+        [Required(ErrorMessage = "You must enter a Game Title")]
         public string GameTitle { get; set; }
-        public int ReleaseYear { get; set; }
+
+        [Display(Name = "Release Year")]
+        public string ReleaseYear { get; set; }
         public string Platform { get; set; }
         public string Publisher { get; set; }
         public bool CompleteCopy { get; set; }
--- VideoGameCatolog/Controllers/HomeController.cs
+++ VideoGameCatolog/Controllers/HomeController.cs
@@ -6,19 +6,61 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using VideoGameCatolog.Models;
+using System.Data.SQLite;
+using DataAccessLibrary;
+using DataAccessLibrary.Models;
 
 namespace VideoGameCatolog.Controllers
 {
     public class HomeController : Controller
     {
+        SQLiteCommand command = new SQLiteCommand();
+        SQLiteDataReader dr;
+        SQLiteConnection connection = new SQLiteConnection();
+        List<VideoGameModel> game = new List<VideoGameModel>();
+        List<ConsoleModel> system = new List<ConsoleModel>();
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            connection.ConnectionString = VideoGameCatolog.Properties.Resources.ConnectionString;
         }
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        private void GetData()
+        {
+            try
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "Select Id, GameTitle, ReleaseYear, Platform, Publisher, CompleteCopy, PhsyicalCopy from VideoGames";
+                command.CommandText = "Select Id, ConsoleName from Console";
+                dr = command.ExecuteReader();
+                while (dr.Read())
+                {
+                    
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public IActionResult AddGame()
+        {
+            return View();
+        }
+
+        public IActionResult RemoveGame()
         {
             return View();
         }
